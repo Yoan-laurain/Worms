10e4ad40915f53305e95574b6ae9bff5dc6a2ced
DevOfSomewhere <keanu.vongue@gmail.com>
2023-12-28 02:32:37 +0100

    Remove Print
    J'ai aussi changer pour que le multithread ne soit pas dépendant de la machine ou elle tourne

--- Spoon/src/Object/Component/SGravityComponent.cpp
+++ Spoon/src/Object/Component/SGravityComponent.cpp
@@ -9,8 +9,6 @@ SGravityComponent::SGravityComponent(SActor* owner) : SComponent(owner), bSimula
 void SGravityComponent::Simulated(bool bSimulate)
 {
 	bSimulateGravity = bSimulate;
-	std::cout << std::this_thread::get_id() << std::endl;
-
 	if (!bSimulateGravity)
 	{
 		Velocity = 0;
@@ -24,10 +22,7 @@ void SGravityComponent::OnUpdate(const float Deltatime)
 		// velocity : m/s
 		// acc : m/s²
 		Velocity.Y += MathLibrary::Gravity*Deltatime;
-		
-#ifdef DEBUG
-		std::cout << std::this_thread::get_id() << " : " << Velocity << std::endl;
-#endif // DEBUG
+
 		if (GetOwner())
 			GetOwner()->SetLocation(GetOwner()->GetLocation() + (Velocity*Deltatime)); // Todo : faire en sorte que le size ne correspond plus au pixel.
 
